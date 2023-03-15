fbc5f9c54251b4cc68e12ea3a5e1be6c2be6dc77
omar-gamzatov <omar-gamzatov@mail.ru>
2023-03-15 16:55:43 +0300

    Conversion to the PSR-12 standard

--- Functions/MessagesFunctions.php
+++ Functions/MessagesFunctions.php
@@ -6,21 +6,25 @@ namespace Egur\Functions;
 /**
  * Выполняет функцию die() с выводом json encoded error
  * @param string $error
- * @return never
  */
-function die_with_error(string $error) 
+function die_with_error(string $error): void
 {
-    die(json_encode(['error' => $error]));
+    if (is_string($error))
+        die(json_encode(['error' => $error]));
+    else
+        die(json_encode(['error' => 'uncaught error']));
 }
 
 /**
  * Выполняет функцию echo() с выводом json encoded error
  * @param string $error
- * @return never
  */
-function echo_error(string $error) 
+function echo_error(string $error): void
 {
-    die(json_encode(['error' => $error]));
+    if (is_string($error))
+        echo(json_encode(['error' => $error]));
+    else
+        echo(json_encode(['message' => 'uncaught error']));
 }
 
 /**
@@ -28,37 +32,47 @@ function echo_error(string $error)
  * @param string $error
  * @return string
  */
-function get_error(string $error) 
+function get_error(string $error): string
 {
-    return json_encode(['error' => $error]);
+    if (is_string($error))
+        return json_encode(['error' => $error]);
+    else
+        return json_encode(['message' => 'uncaught error']);
 }
 
 /**
  * Выполняет функцию die() с выводом json encoded message
  * @param string $message
- * @return never
  */
-function die_with_message(string $message) 
+function die_with_message(string $message): void
 {
-    die(json_encode(['message' => $message]));
+    if (is_string($message))
+        die(json_encode(['message' => $message]));
+    else
+        die(json_encode(['message' => 'uncaught error']));
 }
 
 /**
  * Выполняет функцию echo() с выводом json encoded message
  * @param string $message
- * @return never
  */
-function echo_message(string $message) 
+function echo_message(string $message): void
 {
-    die(json_encode(['message' => $message]));
+    if (is_string($message))
+        echo(json_encode(['message' => $message]));
+    else
+        echo(json_encode(['message' => 'uncaught error']));
 }
 
 /**
  * Возвращает строку с json encoded message
  * @param string $message
- * @return bool|string
+ * @return string
  */
-function get_message(string $message) 
+function get_message(string $message = ''): string
 {
-    return json_encode(['message' => $message]);
+    if (is_string($message))
+        return json_encode(['message' => $message]);
+    else
+        return json_encode(['message' => 'uncaught error']);
 }
